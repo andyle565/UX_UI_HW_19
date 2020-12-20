3ef65eff38f72ad4b4c9ca49634f9bf889ad5536
andyle565 <68405132+andyle565@users.noreply.github.com>
2020-12-20 00:16:37 -0600

    update 3

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log("Your index.js file is loaded correctly!");
 $("button").on("click", function () {
     console.log("Hey a click occurred!");
 });
+function fade() {
+    $('.preloader').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
